605c639ac5a98e0ef0664cacb9bf313ffcf2c86b
Zane <zane@zanematthew.com>
2014-11-09 00:23:08 -0500

    Removing hardcoded zm ref. for menu title and page title

--- zm-settings.php
+++ zm-settings.php
@@ -2,17 +2,20 @@
 
 Class ZM_Settings Extends ZM_Form_Fields {
 
-    public $namespace;
 
     /**
      * WordPress hooks to be ran during init
      *
      * @since 1.0.0
      */
-    public function __construct( $namespace, $paths ){
+    public function __construct( $names=null, $paths=null, $settings=null ){
 
-        $this->namespace = $namespace;
+        $this->namespace = $names['namespace'];
+        $this->menu_title = $names['menu_title'];
+        $this->page_title = $names['page_title'];
+
         $this->dir_url = $paths['dir_url'];
+        $this->settings = $settings;
 
         add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
         add_action( 'admin_init', array( &$this, 'register_settings' ) );
@@ -20,193 +23,13 @@ Class ZM_Settings Extends ZM_Form_Fields {
     }
 
 
+    /**
+     * Return our settings
+     *
+     * @since 1.0.0.
+     */
     public function settings(){
-
-        $settings = array(
-
-            // General
-            'usage' => array(
-                'title' => __( 'Usage', $this->namespace ),
-                'fields' => array(
-                    array(
-                        'id' => 'usage_header',
-                        'title' => __('Usage', $this->namespace ),
-                        'type' => 'header'
-                    ),
-                    array(
-                        'id' => 'usage_description',
-                        'title' => __( 'Description', $this->namespace ),
-                        'desc' => __( 'Thank you for using zM Settings API. Each tab shows in detail how to use this API. All of the functionality this API provides is designed to be filtered using; hooks and filters. Click through each tab to see the default field types, and how to sanitize settings.
-                            ', $this->namespace ),
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'namespace',
-                        'title' => 'Adding the plugin, namespace',
-                        'desc' => '',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'tabs',
-                        'title' => 'Adding tabs',
-                        'desc' => '',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'settings',
-                        'title' => 'Adding settings',
-                        'desc' => '',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'sanitize',
-                        'title' => 'Sanitizing',
-                        'desc' => '',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => '',
-                        'title' => 'Sanitizing per type',
-                        'desc' => 'sanitize per type',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'sanitize per id',
-                        'title' => 'Sanitizing per id/key',
-                        'desc' => '',
-                        'type' => 'desc'
-                    ),
-                    array(
-                        'id' => 'hooks',
-                        'title' => 'Hooks',
-                        'desc' => '',
-                        'type' => 'desc'
-                    )
-                )
-            ),
-
-            // Notifications
-            'default_field_types' => array(
-                'title' => __( 'Default Fields Types', $this->namespace ),
-                'fields' => array(
-                    array(
-                        'id' => 'my_checkbox_id',
-                        'title' => __( 'Checkbox', $this->namespace ),
-                        'type' => 'checkbox',
-                        'desc' => 'This is my description.'
-                    ),
-                    array(
-                        'id' => 'my_checkboxes_id',
-                        'title' => __( 'Checkboxes', $this->namespace ),
-                        'type' => 'checkboxes',
-                        'options' => array(
-                            'foo' => 'Foo',
-                            'bar' => 'Bar'
-                            )
-                    ),
-                    array(
-                        'id' => 'my_radio_id',
-                        'title' => __( 'Radio', $this->namespace ),
-                        'type' => 'radio',
-                        'options' => array(
-                            'foo' => 'Foo',
-                            'bar' => 'Bar'
-                            )
-                    ),
-                    array(
-                        'id' => 'my_id',
-                        'title' => __( 'Text Field', $this->namespace ),
-                        'type' => 'text',
-                        'desc' => 'This is a default text field, it supports any type of value.'
-                    ),
-                    array(
-                        'id' => 'my_id_url',
-                        'title' => __( 'URL Field', $this->namespace ),
-                        'type' => 'url',
-                        'desc' => 'This is a default URL field, type: url, sanitize: esc_url.'
-                    ),
-                    array(
-                        'id' => 'my_id_email',
-                        'title' => __( 'Email Field', $this->namespace ),
-                        'type' => 'email',
-                        'desc' => 'This is a default email field, type: email, sanitize: sanitize_email.'
-                    ),
-                    array(
-                        'id' => 'my_id_hidden',
-                        'title' => __( 'Hidden Field', $this->namespace ),
-                        'type' => 'hidden',
-                        'desc' => 'This is hidden, you can\'t see it unless you view the html source.'
-                    ),
-                    array(
-                        'id' => 'my_textarea_id',
-                        'title' => __( 'Textarea', $this->namespace ),
-                        'type' => 'textarea',
-                        'desc' => 'Default textarea, sanitize: esc_textarea.'
-                    ),
-                    array(
-                        'id' => 'my_textarea_id_css',
-                        'title' => __( 'CSS Textarea', $this->namespace ),
-                        'type' => 'css_textarea',
-                        'desc' => "Default textarea, sanitize: wp_kses( '' )."
-                    ),
-                    array(
-                        'id' => 'my_textarea_id_email',
-                        'title' => __( 'Email Textarea', $this->namespace ),
-                        'type' => 'textarea_emails',
-                        'desc' => "Custom textarea, supports only valid emails, sanitize: wp_kses( '' )."
-                    ),
-                    array(
-                        'id' => 'my_textarea_id_ips',
-                        'title' => __( 'IP Textarea', $this->namespace ),
-                        'type' => 'textarea_ip',
-                        'desc' => "Custom textarea, supports only valid IP address, sanitize: sanitize_ip)."
-                    ),
-                    array(
-                        'id' => 'any_id',
-                        'title' => __( 'Select', $this->namespace ),
-                        'type' => 'select',
-                        'desc' => '<p>This is a sample select, with options. The options are passed in using the <code>options</code> key, with an assigned array like the following; <code>array( [0] =>"", [2] => Sample Page )</code></p>',
-                        'options' => array(
-                                1 => 'Option 1',
-                                2 => 'Option 2'
-                        )
-                    ),
-                    array(
-                        'id' => 'sample_multiselect',
-                        'title' => 'Multi-select',
-                        'type' => 'multiselect',
-                        'options' => array(
-                                1 => 'Option 1',
-                                2 => 'Option 2'
-                            )
-                    ),
-                    array(
-                        'id' => 'some_state',
-                        'title' => 'US State Select',
-                        'type' => 'us_state_select'
-                    ),
-                    array(
-                        'id' => 'my_image',
-                        'title' => 'Upload',
-                        'type' => 'upload'
-                    ),
-                    array(
-                        'id' => 'my_thinkbox_url',
-                        'title' => 'Thickbox',
-                        'type' => 'thickbox_url',
-                        'std' => 'http://zanematthew.com/',
-                        'placeholder' => 'View Entries',
-                        ),
-                    array(
-                        'id' => 'my_touch_time',
-                        'title' => 'Date Time',
-                        'type' => 'touchtime',
-                        )
-                )
-            )
-        );
-
-        return apply_filters( "{$this->namespace}_settings", $settings );
+        return $this->settings;
     }
 
 
@@ -252,39 +75,27 @@ Class ZM_Settings Extends ZM_Form_Fields {
                     'id'          => isset( $field['id'] ) ? $field['id'] : null,
                     'value'       => $value,
                     'options'     => isset( $field['options'] ) ? $field['options'] : '',
-                    'name'        => $this->namespace . '[' . $field['id'] . ']', // ushyee_settings[my_checkbox_id
-                    'title'       => '',
-                    // 'section'     => $id,
-                    // 'desc'        => ! empty( $field['desc'] ) ? $field['desc'] : '',
-                    // Since we don't want the extended form class to derive names, we specify our names
-                    // 'size'        => isset( $field['size'] ) ? $field['size'] : null,
-                    // 'std'         => isset( $field['std'] ) ? $field['std'] : '',
-                    // 'placeholder' => isset( $field['placeholder'] ) ? $field['placeholder'] : '',
-                    // 'field_class' => isset( $field['field_class'] ) ? $field['field_class'] : '',
-                    // 'rows'        => isset( $field['rows'] ) ? $field['rows'] : ''
+                    'name'        => $this->namespace . '[' . $field['id'] . ']',
+                    'title'       => ''
                 ); // These are extra params based into our function/method
 
                 $final = array_merge( $attr, $temp );
 
                 add_settings_field(
                     $this->namespace.'[' . $field['id'] . ']', // ID
-                    $title,
+                    $title, // Title
 
                     method_exists( $this, 'do_' . $field['type'] )
                         ? array( $this, 'do_' . $field['type'] )
-                        : array( $this, 'missing_callback' ),
+                        : array( $this, 'missing_callback' ), // Callback
 
                     $this->namespace . '_' . $id, // Page
                     $this->namespace . '_' . $id, // Section
                     $final
                 );
             }
-
         }
 
-        // Creates our settings in the options table
-        // Note this uses zm_forms, but doesn't use the zm_forms sanitize method,
-        // rather it uses its own
         register_setting( $this->namespace, $this->namespace, array( &$this, 'sanitize' ) );
     }
 
@@ -299,8 +110,8 @@ Class ZM_Settings Extends ZM_Form_Fields {
         $sub_menu_pages = apply_filters( $this->namespace . '_admin_submenu', array(
             array(
                 'parent_slug' => 'options-general.php',
-                'page_title' => __( 'ZM Settings', $this->namespace  ),
-                'menu_title' => __( 'ZM Settings', $this->namespace  ),
+                'page_title' => $this->page_title,
+                'menu_title' => $this->menu_title,
                 'capability' => 'manage_options',
                 'menu_slug' => $this->namespace,
                 'function' => 'load_template'
@@ -358,7 +169,7 @@ Class ZM_Settings Extends ZM_Form_Fields {
         } ?>
         <div class="wrap">
             <div id="icon-options-general" class="icon32"><br></div>
-            <h2><?php echo apply_filters( "{$this->namespace}_page_title", 'ZM Settings API' ); ?></h2>
+            <h2><?php echo $this->page_title; ?></h2>
             <form action="options.php" method="post" class="form">
 
                 <h2 class="nav-tab-wrapper">
@@ -469,7 +280,6 @@ Class ZM_Settings Extends ZM_Form_Fields {
                         case 'textarea_email_template' :
                         case 'checkbox' :
                         case 'radio' :
-                        case 'touchtime':
                             $input[ $key ] = $this->sanitize_default( $value );
                             break;
 
@@ -487,6 +297,10 @@ Class ZM_Settings Extends ZM_Form_Fields {
 
                         case 'textarea_ip' :
                             $input[ $key ] = $this->sanitize_textarea_ip( $value );
+                            break;
+
+                        case 'touchtime' :
+                            $input[ $key ] = $this->sanitize_touchtime( $value );
                             break;
 
                         default:
@@ -558,10 +372,17 @@ Class ZM_Settings Extends ZM_Form_Fields {
     }
 
 
+    /**
+     * Load our JS, CSS files
+     *
+     * @since 1.0.0
+     * @param array $args Arguments passed by the setting
+     * @return void
+     */
     public function admin_enqueue_scripts(){
         $screen = get_current_screen();
         if ( $screen->id == 'settings_page_' . $this->namespace ){
-            wp_enqueue_style( 'zm-settings-admin-script', $this->dir_url . 'assets/stylesheets/admin.css', '', '1.0' );
+            wp_enqueue_style( $this->namespace . 'admin-script', $this->dir_url . 'assets/stylesheets/admin.css', '', '1.0' );
         }
     }
 }
